a74b05c41f84617973f6232ee9213ce2c9814e6d
jetson <jstone14@terpmail.umd.edu>
2023-08-16 17:14:02 -0400

    docker start upgrade

--- ml4all.sh
+++ ml4all.sh
@@ -5,6 +5,13 @@ read name
 echo "Enter your section":
 read sec
 
-mkdir -p ~/ML4ALL/mount_points/${name}_${sec}/ && cp -r ~/ML4ALL/lesson_material/* ~/ML4ALL/mount_points/${name}_${sec}
+if [[ ! -d ~/ML4ALL/mount_points/${name}_${sec}/ ]]; then
+	echo "Mount point not found - creating ..."
+	mkdir -p ~/ML4ALL/mount_points/${name}_${sec}/
+	cp -r ~/ML4ALL/lesson_material/* ~/ML4ALL/mount_points/${name}_${sec}
+else
+	echo "Mount point already exists!"
+fi
+
 echo "Starting Docker with image v2.0.2-r32.7.1"
 sudo docker run --runtime nvidia -it --rm --network host --volume ~/ML4ALL/mount_points/${name}_${sec}/:/nvdli-nano/ --device /dev/video0 nvcr.io/nvidia/dli/dli-nano-ai:v2.0.2-r32.7.1
