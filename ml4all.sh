2bd46007286aedb6bb35d141072856441d7e1ca4
jeronstone <jstone14@terpmail.umd.edu>
2023-08-29 12:01:33 -0400

    test git pull

--- ml4all.sh
+++ ml4all.sh
@@ -24,7 +24,7 @@ else
 fi
 
 echo "Starting Docker with image ${NVDLI_DOCKER_IMAGE_VER}"
-exit 0 
+echo "LETS GOOOOOOO"
 
 echo "RECOMMENDED: Serial connection @ 192.168.55.1:8888 (password dlinano)"
 sudo docker run --runtime nvidia -it --rm --network host --volume ~/ML4ALL/mount_points/${name}_${sec}/:/nvdli-nano/ --device /dev/video0 nvcr.io/nvidia/dli/dli-nano-ai:${NVDLI_DOCKER_IMAGE_VER}
