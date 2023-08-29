524117ca7230847bf13b86d000223a8829000fb9
jeronstone <jstone14@terpmail.umd.edu>
2023-08-29 11:59:01 -0400

    nvdli docker image version tag

--- ml4all.sh
+++ ml4all.sh
@@ -13,6 +13,8 @@ IFS= read -r -p 'Enter your section: ' sec
 name=${name// /_}
 sec=${sec// /_}
 
+export NVDLI_DOCKER_IMAGE_VER=v2.0.2-r32.7.1
+
 if [[ ! -d ~/ML4ALL/mount_points/${name}_${sec}/ ]]; then
 	echo "Mount point not found - creating ..."
 	mkdir -p ~/ML4ALL/mount_points/${name}_${sec}/
@@ -21,6 +23,8 @@ else
 	echo "Mount point already exists!"
 fi
 
-echo "Starting Docker with image v2.0.2-r32.7.1"
+echo "Starting Docker with image $NVDLI_DOCKER_IMAGE_VER"
+exit 0 
+
 echo "RECOMMENDED: Serial connection @ 192.168.55.1:8888 (password dlinano)"
-sudo docker run --runtime nvidia -it --rm --network host --volume ~/ML4ALL/mount_points/${name}_${sec}/:/nvdli-nano/ --device /dev/video0 nvcr.io/nvidia/dli/dli-nano-ai:v2.0.2-r32.7.1
+sudo docker run --runtime nvidia -it --rm --network host --volume ~/ML4ALL/mount_points/${name}_${sec}/:/nvdli-nano/ --device /dev/video0 nvcr.io/nvidia/dli/dli-nano-ai:$NVDLI_DOCKER_IMAGE_VER
