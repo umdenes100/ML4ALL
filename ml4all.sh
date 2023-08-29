c96bc837ae5e7f65bec98fe0b950977687dc9c5d
jeronstone <jstone14@terpmail.umd.edu>
2023-08-29 12:37:57 -0400

    tests

--- ml4all.sh
+++ ml4all.sh
@@ -4,16 +4,20 @@ export DEV_SCRIPT_PATH=~/ML4ALL/dev_nb.sh
 export STUDENT_SCRIPT_PATH=~/ML4ALL/student_nb.sh
 
 if [ -z "$1" ] ; then
+	echo "STUDENT!!!"
 	GIT_PULL_EN=1
 	DEV_NB=0
 elif [ $1 = "dev" ] ; then
+	echo "DEV!!!"
 	DEV_NB=1
 	if [ -z "$2" ] && [ $2 = "nogit" ] ; then
+		echo "no git dev"
 		GIT_PULL_EN=0
 	else
 		GIT_PULL_EN=1
 	fi
 elif [ $1 = "nogit" ] ; then
+	echo "no git student"
 	GIT_PULL_EN=0
 fi
 
