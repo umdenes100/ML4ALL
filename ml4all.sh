7147488090b91b124ec91f036d616c6b8e53c4c8
jeronstone <jstone14@terpmail.umd.edu>
2023-08-29 12:11:08 -0400

    script overhaul 1

--- ml4all.sh
+++ ml4all.sh
@@ -1,30 +1,22 @@
 #!/bin/bash
 
 if [ -z "$1" ] ; then
+	export GIT_PULL_EN=1
+elif [ $1 = "dev" ] ; then
+	export GIT_PULL_EN=1
+	export DEV_NB=1
+fi
+
+if [ ${GIT_PULL_EN} = "1" ] ; then
 	echo "Fetching latest from repo ..."
 	cd ~/ML4ALL ; git pull || echo "git pull failed"
 	echo "Done ..." ; echo ""
-else
+else 
 	echo "Skipping git pull" ; echo ""
 fi
 
-IFS= read -r -p 'Enter your name: ' name
-IFS= read -r -p 'Enter your section: ' sec
-name=${name// /_}
-sec=${sec// /_}
-
-export NVDLI_DOCKER_IMAGE_VER=v2.0.2-r32.7.1
-
-if [[ ! -d ~/ML4ALL/mount_points/${name}_${sec}/ ]]; then
-	echo "Mount point not found - creating ..."
-	mkdir -p ~/ML4ALL/mount_points/${name}_${sec}/
-	cp -r ~/ML4ALL/lesson_material/* ~/ML4ALL/mount_points/${name}_${sec}
+if [${DEV_NB} = "1"] ; then
+	~/ML4ALL/dev_nb.sh
 else
-	echo "Mount point already exists!"
+	~/ML4ALL/student_nb.sh
 fi
-
-echo "Starting Docker with image ${NVDLI_DOCKER_IMAGE_VER}"
-echo "LETS GOOOOOOO"
-
-echo "RECOMMENDED: Serial connection @ 192.168.55.1:8888 (password dlinano)"
-sudo docker run --runtime nvidia -it --rm --network host --volume ~/ML4ALL/mount_points/${name}_${sec}/:/nvdli-nano/ --device /dev/video0 nvcr.io/nvidia/dli/dli-nano-ai:${NVDLI_DOCKER_IMAGE_VER}
---
+++ student_nb.sh
@@ -0,0 +1,20 @@
+IFS= read -r -p 'Enter your name: ' name
+IFS= read -r -p 'Enter your section: ' sec
+name=${name// /_}
+sec=${sec// /_}
+
+export NVDLI_DOCKER_IMAGE_VER=v2.0.2-r32.7.1
+
+if [[ ! -d ~/ML4ALL/mount_points/${name}_${sec}/ ]]; then
+	echo "Mount point not found - creating ..."
+	mkdir -p ~/ML4ALL/mount_points/${name}_${sec}/
+	cp -r ~/ML4ALL/lesson_material/* ~/ML4ALL/mount_points/${name}_${sec}
+else
+	echo "Mount point already exists!"
+fi
+
+echo "Starting Docker with image ${NVDLI_DOCKER_IMAGE_VER}"
+echo "LETS GOOOOOOO"
+
+echo "RECOMMENDED: Serial connection @ 192.168.55.1:8888 (password dlinano)"
+sudo docker run --runtime nvidia -it --rm --network host --volume ~/ML4ALL/mount_points/${name}_${sec}/:/nvdli-nano/ --device /dev/video0 nvcr.io/nvidia/dli/dli-nano-ai:${NVDLI_DOCKER_IMAGE_VER}
